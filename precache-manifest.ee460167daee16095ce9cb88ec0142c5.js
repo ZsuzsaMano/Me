3cd80b2f8e6fedf3be03d244c622d65e923a7b8c
Mano <manoaway@gmail.com>
2019-11-18 11:36:42 +0100

    Updates

---
+++ precache-manifest.ee460167daee16095ce9cb88ec0142c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004fa3cc8a49acba730b3d96ed6be210",
+    "url": "/Me/index.html"
+  },
+  {
+    "revision": "213d846104c35380a647",
+    "url": "/Me/static/css/main.072b38e5.chunk.css"
+  },
+  {
+    "revision": "447f2957b65c10491fba",
+    "url": "/Me/static/js/2.b1bfcbc7.chunk.js"
+  },
+  {
+    "revision": "213d846104c35380a647",
+    "url": "/Me/static/js/main.a6629c5f.chunk.js"
+  },
+  {
+    "revision": "b92d12dd791468327ba2",
+    "url": "/Me/static/js/runtime-main.925d9915.js"
+  }
+]);
